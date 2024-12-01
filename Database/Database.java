3e3f93856b4e176c67dd3db31a9997065523c3a9
Naira05 <162843696+Naira05@users.noreply.github.com>
2024-12-01 14:46:52 +0200

    innit commit

--- Database/Database.java
+++ Database/Database.java
@@ -10,6 +10,7 @@ public class Database {
     public static List<Product> products= new ArrayList<>();
     public static List<Order> orders= new ArrayList<>();
     public static List<Category>categories= new ArrayList<>();
+    public static List<Cart> carts = new ArrayList<>();
     public static List<Admin>admins=new ArrayList<>();
 
     static{
